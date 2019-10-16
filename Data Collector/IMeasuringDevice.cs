34f36a4a601ddaa5ca430f0439e08447f8a87853
mike <11082686+onsokumaru@users.noreply.github.com>
2019-10-15 20:43:17 -0400

    Added stubs for interface IMeasuringDevices

---
+++ Data Collector/IMeasuringDevice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Collector
+{
+    interface IMeasuringDevice
+    {
+        // method declarations
+        public decimal MetricValue()
+        {
+            // This method will return a decimal that represents the 
+            // metric value of the most recent measurement that was captured
+
+        }
+
+        public decimal ImperialValue()
+        {
+            // This method will return a decimal that represents the imperial 
+            // value of the most recent measurement that was captured
+
+        }
+
+        public void StartCollecting()
+        {
+            // This method will start the device running. 
+            // It will begin collecting measurements and record them 
+
+        }
+
+        public void StopCollecting()
+        {
+            // This method will stop the device. 
+            // It will cease collecting measurements
+
+        }
+
+        public int[] GetRawData()
+        {
+            // This method will retrieve a copy of all of the recent data 
+            // that the measuring device has captured. The data will be returned 
+            // as an array of integer values
+
+        }
+    }
+}
