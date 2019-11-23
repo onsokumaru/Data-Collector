473f851bd8c2d6f8e6091edf9e121f1ffd52cdf2
mike <11082686+onsokumaru@users.noreply.github.com>
2019-11-23 17:50:19 -0500

    Fleshed out Device class

--- Data Collector/Device.cs
+++ Data Collector/Device.cs
@@ -8,13 +8,17 @@ namespace Data_Collector
 {
     class Device
     {
+        public Device() { } // maybe not necessary, I think c# atuo creates a default constructor
         // method declarations
 
         public int GetMeasurement()
         {
             // This method will return a random integer between 1 and 10 as a
             // measurement of some imaginary object.
-            return;
+
+            Random rand = new Random();
+            return rand.Next(1, 11);
+            
         }
     }
 }
