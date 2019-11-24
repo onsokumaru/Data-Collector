2d8468fd0f215a48b8b46ffb602218d0c894f5cf
mike <11082686+onsokumaru@users.noreply.github.com>
2019-11-24 17:55:25 -0500

    Added FixedSizeQueue.cs, added code to Device.cs

--- Data Collector/Device.cs
+++ Data Collector/Device.cs
@@ -3,21 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Data_Collector
 {
     class Device
     {
-        public Device() { } // maybe not necessary, I think c# atuo creates a default constructor
+        private Timer timer;    // declare timer object
+        private int data = 0;   // declare variable to hold randomly generated value for GetMeasurement() to return 
+        FixedSizeQueue<int> myData = new FixedSizeQueue<int>(); // see class declaration for description
+        Random rand = new Random();     // create new instance of Random to geneate measurements
+
+        // constructor
+       
+        public Device() 
+        {
+            myData.Limit = 10;
+            // create timer event - getting error that Timer does not have constructor that takes 4 arguments
+            // going to try something different
+            // timer = new Timer(timer_Tick, null, (int)TimeSpan.FromSeconds(1).TotalMilliseconds, (int)TimeSpan.FromSeconds(15).TotalMilliseconds);
+            timer = new Timer(15000);
+            // fire elapsed event when timer expires
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+
+        }
+
         // method declarations
 
         public int GetMeasurement()
         {
-            // This method will return a random integer between 1 and 10 as a
-            // measurement of some imaginary object.
+            return data;
+        }
 
-            Random rand = new Random();
-            return rand.Next(1, 11);
+        public string History => PrintValues(myData);
+
+        public string PrintValues(FixedSizeQueue<int> myQueue)
+        {
+            StringBuilder myString = new StringBuilder();
+            foreach ( var i in myQueue.q)
+            {
+                myString.AppendLine(i.ToString());
+            }
+
+            return myString.ToString();
+        }
+
+        // commenting out because of error message in timer creaetion in Device constructor 
+        // going to try it a differnt way
+
+        //private async void timer_Tick(object state)
+        //{
+        //    // randomly generate new random number and update variable data
+        //    data = rand.Next(1, 11);
+        //    myData.Enqueue(data);
+        //}
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            // example from docs.microsoft.com
+            data = rand.Next(1, 11);
+            myData.Enqueue(data);
             
         }
     }
---
+++ Data Collector/FixedSizeQueue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+
+namespace Data_Collector
+{
+    internal class FixedSizeQueue<T>    // code for fixed size queue that auto-dequeues when limit reached
+    {
+        public ConcurrentQueue<T> q = new ConcurrentQueue<T>();
+        private object lockObject = new object();
+        public int Limit { get; set; }
+
+        // enqueue method to automatically dequeue when limit reached
+        public void Enqueue(T obj)
+        {
+            q.Enqueue(obj);
+            lock (lockObject)
+            {
+                T overflow;
+                while (q.Count > Limit && q.TryDequeue(out overflow)) ;
+            }
+        }
+        
+    }
+}
